33e23ecd37f1a5a3d8f16bdad24c10522232c0c6
lex <lex24@ukr.net>
2014-04-07 12:34:52 +0300

    configured index.php for autocomplete

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <?php
-
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii/framework/yii.php';
+require(dirname(__FILE__) . '/../framework/YiiBase.php');
+/**
+ * To configure using autocomplete install YiiStrom plugin
+ * check http://www.yiiframework.ru/doc/cookbook/ru/ide
+ * check http://rmcreative.ru/blog/post/yii--avtokomplit-dlja-yiiapp
+ * File → Settings → Project settings → PHP → PHP Home → Add. Указываем путь до директории framework.
+ */
+class Yii extends YiiBase {
+    /**
+     * @static
+     * @return CWebApplication
+     */
+    public static function app()
+    {
+        return parent::app();
+    }
+}
+
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
@@ -9,5 +25,4 @@ defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
-require_once($yii);
 Yii::createWebApplication($config)->run();
